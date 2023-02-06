1d65d158c34bf914b789b96b85518ccd5a83a68e
Travis Hubbard <travis.hubbard@gmail.com>
2023-02-06 17:53:19 -0500

    updated templates

--- run.py
+++ run.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 @app.route("/index")
 @app.route('/')
 def index_template():
-	return render_template('index.html')
+	return render_template('index.html', title='Vegas Tracker')
 
 @app.route('/about')
 def about_template():
-	return render_template('about.html')
+	return render_template('about.html', title='About')
 
 @app.route('/future')
 def future_template():
-	return render_template('future.html')
+	return render_template('future.html', title='Futures')
 
 if __name__ =='__main__':
 	app.run(debug=True)
